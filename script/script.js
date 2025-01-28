d5ad3cb473a1a812519ff87da7deb719282465d3
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2025-01-28 10:40:50 -0300

    -

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const hamburguer = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener('click', () => {
+hamburguer.addEventListener('click', () => {
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
